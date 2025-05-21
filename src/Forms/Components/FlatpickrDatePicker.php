054ca0749c3c609e6b850f1b54dd6ca31f42fd1b
Dinakeri <117737175+Dinakeri@users.noreply.github.com>
2025-05-21 09:40:23 +0200

    DefaultDate added
    Added a DefaultDate to default your date!

--- src/Forms/Components/FlatpickrDatePicker.php
+++ src/Forms/Components/FlatpickrDatePicker.php
@@ -18,6 +18,8 @@ class FlatpickrDatePicker extends Field
 
     protected bool $shouldEnableSeconds = false;
 
+    protected $defaultDate = null;
+
     protected string $flatpickrDateFormat = 'Y-m-d H:i:s';
 
     protected string $flatpickrMode = 'single';
@@ -35,6 +37,15 @@ class FlatpickrDatePicker extends Field
         $config['enableSeconds'] = $this->shouldEnableSeconds;
         $config['mode'] = $this->flatpickrMode;
 
+            if ($this->defaultDate !== null) {
+        // Flatpickr converteert dit zelf, als het formaat klopt.
+        $config['defaultDate'] = $this->defaultDate;
+    } else {
+        // Anders gebruikt Flatpickr de 'value' attribute van de input field,
+        // wat Filament's $getState() is.
+        // Als je hier expliciet niets meegeeft, vertrouwt het op de input value.
+    }
+
         return $config;
     }
 
@@ -43,7 +54,6 @@ class FlatpickrDatePicker extends Field
     public function disableWeekends(bool $condition = true): static
     {
         $this->shouldDisableWeekends = $condition;
-
         return $this;
     }
 
@@ -58,7 +68,6 @@ class FlatpickrDatePicker extends Field
             $this->getExtraAttributes(),
             ['data-disabled-days' => implode(',', $days)]
         ));
-
         return $this;
     }
 
@@ -67,7 +76,6 @@ class FlatpickrDatePicker extends Field
     public function disableDates(array $dates): static
     {
         $this->disabledDates = $dates;
-
         return $this->extraAttributes(array_merge(
             $this->getExtraAttributes(),
             ['data-disabled-dates' => json_encode($dates)]
@@ -82,49 +90,48 @@ class FlatpickrDatePicker extends Field
     public function weekNumbers(bool $condition = true): static
     {
         $this->shouldShowWeekNumbers = $condition;
-
         return $this;
     }
 
     public function enableTime(bool $condition = true): static
     {
         $this->shouldEnableTime = $condition;
-
         return $this;
     }
 
     public function time24hr(bool $condition = true): static
     {
         $this->shouldEnable24hrTime = $condition;
-
         return $this;
     }
 
     public function noCalendar(bool $condition = true): static
     {
         $this->shouldShowNoCalendar = $condition;
-
         return $this;
     }
 
     public function enableSeconds(bool $condition = true): static
     {
         $this->shouldEnableSeconds = $condition;
-
         return $this;
     }
 
     public function dateFormat(string $format): static
     {
         $this->flatpickrDateFormat = $format;
-
         return $this;
     }
 
     public function mode(string $mode): static
     {
         $this->flatpickrMode = $mode;
+        return $this;
+    }
 
+    public function defaultDate($date): static
+    {
+        $this->defaultDate = $date;
         return $this;
     }
 }
